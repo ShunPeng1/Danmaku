9ddbc05423be8efca270c9f36e056809c688dec5
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-04-04 08:18:39 +0700

    feat: phantasm boss

---
+++ Assets/_Scripts/CoreGame/InteractionSystems/Roles/TruePhantasmBossDanmakuRole.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using _Scripts.CoreGame.InteractionSystems.Interfaces;
+
+namespace _Scripts.CoreGame.InteractionSystems.Roles
+{
+    public class TruePhantasmBossDanmakuRole : IDanmakuRole
+    {
+        private DanmakuPlayerSubsystem _danmakuPlayerSubsystem;
+        private bool _isRevealed;
+
+        bool IDanmakuRole.IsRevealed
+        {
+            get => _isRevealed;
+            set => _isRevealed = value;
+        }
+
+        DanmakuPlayerSubsystem IDanmakuRole.DanmakuPlayerSubsystem
+        {
+            get => _danmakuPlayerSubsystem;
+            set => _danmakuPlayerSubsystem = value;
+        }
+        
+        public bool HasRole(DanmakuRoleEnum danmakuRoleEnum)
+        {
+            return DanmakuRoleEnum.ExtraBoss == danmakuRoleEnum;
+        }
+
+
+        public int MinPlayerCountForAvailable() => 4;
+
+        public bool IsGoalReached()
+        {
+            var stageBossOrHeroinePlayers = _danmakuPlayerSubsystem.Players.FindAll(player => player.Role.HasRole(DanmakuRoleEnum.StageBoss) || player.Role.HasRole(DanmakuRoleEnum.Heroine));
+
+            return stageBossOrHeroinePlayers.All(bossPlayer => !bossPlayer.IsAlive);
+        }
+
+        public bool CanRevealRole()
+        {
+            return false;
+        }
+
+        public void RevealRole()
+        {
+            _isRevealed = true;
+        }
+    }
+}
