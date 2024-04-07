ba04631b2c1be07c82408863b2bc28ab9b8cda3f
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-04-07 22:12:32 +0700

    file: import simulation Manager

---
+++ Assets/_Scripts/BaseGame/InteractionSystems/Simulation/SimulationManager.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Priority_Queue;
+using Shun_Utilities;
+using UnityEngine;
+
+
+namespace _Scripts.Simulation
+{
+    public class SimulationManager : SingletonMonoBehaviour<SimulationManager>
+    {
+        private SimplePriorityQueue<SimulationPackage> _simulationQueue = new();
+
+
+        private bool _isExecuting;
+
+        public void AddSimulationPackage(SimulationPackage simulationPackage)
+        {
+            if (simulationPackage == null || _simulationQueue.Contains(simulationPackage))
+            {
+                return;
+            }
+            _simulationQueue.Enqueue(simulationPackage, simulationPackage.Priority);
+        }
+
+        public void RemoveCoroutineSimulationObject(SimulationPackage simulationPackage)
+        {
+            if (simulationPackage == null || !_simulationQueue.Contains(simulationPackage))
+            {
+                return;
+            }
+            _simulationQueue.Remove(simulationPackage);
+        }
+
+        public void ClearAll()
+        {
+            _simulationQueue.Clear();
+        }
+
+        private void FixedUpdate()
+        {
+            StartCoroutine(ExecuteAll());
+        }
+        
+
+        IEnumerator ExecuteAll()
+        {
+            if (_isExecuting)
+            {
+                yield break;
+            }
+
+            _isExecuting = true;
+            while (_simulationQueue.Count > 0)
+            {
+                var simulationObject = _simulationQueue.Dequeue();
+                yield return StartCoroutine(ExecuteCoroutineSimulationConcurrent(simulationObject));
+            }
+
+            _isExecuting = false;
+        }
+        IEnumerator ExecuteCoroutineSimulationConcurrent(SimulationPackage simulationPackage)
+        {
+            foreach (var enumerator in simulationPackage.ExecuteEvents)
+            {
+                if (enumerator == null) continue;
+                yield return StartCoroutine(enumerator.Invoke());
+            }
+
+            yield return null;
+}
+        
+        IEnumerator ExecuteCoroutineSimulationParallel(SimulationPackage simulationPackage)
+        {
+            List<Coroutine> runningCoroutines = new List<Coroutine>();
+
+            foreach (var enumerator in simulationPackage.ExecuteEvents)
+            {
+                if (enumerator == null) continue;
+                Coroutine newCoroutine = StartCoroutine(enumerator.Invoke());
+                runningCoroutines.Add(newCoroutine);
+            }
+
+            // WaitPhase for all running coroutines to complete
+            yield return WaitAllCoroutines(runningCoroutines);
+        }
+
+        IEnumerator WaitAllCoroutines(List<Coroutine> coroutines)
+        {
+            foreach (var coroutine in coroutines)
+            {
+                yield return coroutine;
+            }
+        }
+    }
+}
+
---
+++ Assets/_Scripts/BaseGame/InteractionSystems/Simulation/SimulationPackage.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+namespace _Scripts.Simulation
+{
+    public class SimulationPackage
+    {
+        public float Priority { get; set; }
+        public readonly List<Func<IEnumerator>> ExecuteEvents = new();
+
+        public SimulationPackage(float priority = 0)
+        {
+            Priority = priority;
+        }
+
+        public void AddToPackage(float waitTime)
+        {
+            IEnumerator CoroutineWrapper()
+            {
+                yield return new WaitForSeconds(waitTime);
+            }
+            
+            ExecuteEvents.Add(CoroutineWrapper);
+        }
+        
+        public void AddToPackage(Action action)
+        {
+            ExecuteEvents.Add(ConvertToIEnumerator(action));
+        }
+
+        public void AddToPackage(Func<IEnumerator> coroutine)
+        {
+            ExecuteEvents.Add(coroutine);
+        }
+        
+        public void AddToPackage(IEnumerator coroutine)
+        {
+            IEnumerator CoroutineWrapper() => coroutine;
+            ExecuteEvents.Add(CoroutineWrapper);
+        }
+
+        public void AddToPackage(Tween tween)
+        {
+            ExecuteEvents.Add(ConvertToIEnumerator(tween));
+        }
+
+        private Func<IEnumerator> ConvertToIEnumerator(Action action)
+        {
+            return new Func<IEnumerator>(() =>
+            {
+                IEnumerator CoroutineWrapper()
+                {
+                    action();
+                    yield return null; // Yielding once to make it a valid coroutine
+                }
+
+                return CoroutineWrapper();
+            });
+        }
+
+        private Func<IEnumerator> ConvertToIEnumerator(Tween tween)
+        {
+            if (tween == null || !tween.IsActive()) return null;
+            
+            tween.Pause();
+            return new Func<IEnumerator>(() =>
+            {
+                IEnumerator CoroutineWrapper()
+                {
+                    tween.SetAutoKill(false);
+                    tween.Play();
+                    while (!tween.IsComplete())
+                    {
+                        yield return null;
+                    }
+                    tween.Kill();
+                }
+
+                return CoroutineWrapper();
+            });
+        }
+        
+        
+    }
+}
