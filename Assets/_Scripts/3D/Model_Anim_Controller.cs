b184cc5e323a9da5bed681605aeda224dcf07a47
VietLQ1 <vietdz145@gmail.com>
2024-04-19 15:51:32 +0700

    feat: 3D model animation

---
+++ Assets/_Scripts/3D/Model_Anim_Controller.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Model_Anim_Controller : MonoBehaviour
+{
+    Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnAttackAnimation()
+    {
+        _animator.SetTrigger("Attack");
+    }
+    public void OnDestroy()
+    {
+        _animator.SetTrigger("Defeat");
+    }
+}
