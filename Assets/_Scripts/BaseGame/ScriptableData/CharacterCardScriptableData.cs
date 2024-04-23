7041d7e123b833f29b9a08e1fa124f9ffeafdab2
VietLQ1 <vietdz145@gmail.com>
2024-04-23 15:35:47 +0700

    feat: Character Scriptable and Model Loader

---
+++ Assets/_Scripts/BaseGame/ScriptableData/CharacterCardScriptableData.cs
@@ -0,0 +1,24 @@
+using System;
+using _Scripts.CoreGame.InteractionSystems.Interfaces;
+using UnityEngine;
+
+
+namespace _Scripts.BaseGame.ScriptableData
+{
+    [CreateAssetMenu(fileName = "CharacterCardScriptableData", menuName = "ScriptableData/CharacterCardScriptableData")]
+    public class CharacterCardScriptableData : ScriptableObject
+    {
+        public string CardName;
+        public string CardTitle;
+        public CardDeckEnum CardDeck;
+        public CardExpansionEnum CardExpansion;
+        public CardSeasonEnum CardSeason;
+
+        public Sprite CardIllustration;
+
+        public CardRuleScriptableData[] AbilityCardRules;
+        public CardRuleScriptableData[] SpellCardRules;
+
+        public GameObject ModelData;
+    }
+}
