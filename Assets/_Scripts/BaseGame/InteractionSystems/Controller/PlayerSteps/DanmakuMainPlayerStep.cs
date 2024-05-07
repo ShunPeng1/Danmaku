5909a39561b433634edd4e102a84fc5f3fe15c47
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-05-07 15:57:23 +0700

    feat: play card step

--- Assets/_Scripts/BaseGame/InteractionSystems/Controller/PlayerSteps/DanmakuMainPlayerStep.cs
+++ Assets/_Scripts/BaseGame/InteractionSystems/Controller/PlayerSteps/DanmakuMainPlayerStep.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using _Scripts.BaseGame.InteractionSystems.Interfaces;
 using _Scripts.BaseGame.Views;
+using Shun_Utilities;
 using UnityEngine;
 
 namespace _Scripts.CoreGame.InteractionSystems.GameSteps
@@ -16,12 +18,24 @@ namespace _Scripts.CoreGame.InteractionSystems.GameSteps
         public void Execute(DanmakuInteractionController interactionController, DanmakuPlayerModel playerModel, DanmakuPlayerBaseView playerView, Action finishExecuteCallback = null)
         {
             Debug.Log(playerModel.PlayerId + " Main Step Executed!");
-        
+            
+            var menus = new ObservableList<DanmakuSessionMenu>();
+            
             var session = new DanmakuSession.Builder()
                 .WithPlayingPlayerModel(new List<IDanmakuActivator>(){playerModel})
+                .WithPlayingSessionMenus(menus)
                 .WithPlayerSessionKindEnum(EndSessionKindEnum.NonePlayed)
                 .WithCountDownTime(1000f)
                 .Build(interactionController);
+            var choices = new List<DanmakuSessionChoice>();
+            var menu = new DanmakuSessionMenu(session, playerModel, choices);
+            menus.Add(menu);
+
+            choices.Add(new DanmakuSessionChoice(
+                menu, 
+                new List<IDanmakuTargetable>(playerModel.CardHandModel.Cards.Items.ToList()),
+                ChoiceActionEnum.Confirm
+                ));
 
             session.OnSessionStartEvent.Subscribe(()=>playerView.SessionHandler.SetCurrentSession(session));
             session.SubscribeOnSessionEnd(finishExecuteCallback, true);
