7ecd948adb3a503c197de7200bbc7db4c47c1d07
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-05-29 14:23:58 +0700

    fix: T POSE

--- Assets/_Scripts/BaseGame/Views/VR/Views/VRCharacterView.cs
+++ Assets/_Scripts/BaseGame/Views/VR/Views/VRCharacterView.cs
@@ -1,4 +1,5 @@
-﻿using _Scripts.CoreGame.InteractionSystems;
+﻿using _Scripts.BaseGame.ScriptableData;
+using _Scripts.CoreGame.InteractionSystems;
 using UnityEngine;
 
 namespace _Scripts.BaseGame.Views.Basics
@@ -6,11 +7,36 @@ namespace _Scripts.BaseGame.Views.Basics
     public class VRCharacterView : DanmakuCharacterBaseView
     {
         [SerializeField] private Transform _characterTransform;
-        
+        [SerializeField] RuntimeAnimatorController _controller;
+
+        private CharacterCardScriptableData _characterData;
         private GameObject _characterObject;
+        
+        
         public override void SetupCharacter(DanmakuCharacterCardModel chosenCard)
         {
-            _characterObject = Instantiate(chosenCard.CharacterCardData.ModelData, _characterTransform);
+            //_characterObject = Instantiate(chosenCard.CharacterCardData.ModelData, _characterTransform);
+            
+            _characterData = chosenCard.CharacterCardData;
+            LoadModel(chosenCard.CharacterCardData);
+        }
+        
+        // Update is called once per frame
+        public void LoadModel(CharacterCardScriptableData characterCardScriptableData)
+        {
+            GameObject modelPrefab = _characterData.ModelData;
+            if (modelPrefab == null) return;
+            
+            
+            GameObject loadedPrefab = Instantiate(modelPrefab, _characterTransform);
+            Animator animator = loadedPrefab.GetComponent<Animator>();
+            if (animator != null )
+            {
+                animator.runtimeAnimatorController = _controller;
+                loadedPrefab.AddComponent<ModelAnimController>();
+            }
+                
+            _characterObject = loadedPrefab;
         }
     }
 }
